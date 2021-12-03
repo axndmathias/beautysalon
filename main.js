39c3e11a9baefda0ce7a73334c93f635fc158f7f
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-12-03 23:29:46 +0100

    added scrollreveal

--- main.js
+++ main.js
@@ -42,3 +42,23 @@ const swiper = new Swiper('.swiper', {
     keyboard: true
 })
 
+
+/* scrollreveal: Mostar elementos scroll na pagina*/
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links
+`, 
+{
+interval: 100
+})
